167a87d7f6357f4930fadd80df32a2c93e509773
Avijeet <avijeetpandey25@gmail.com>
2024-12-16 02:00:35 +0530

    add: get book by ID

--- books.py
+++ books.py
@@ -4,26 +4,31 @@ app = FastAPI()
 
 BOOKS = [
     {
+        "id": 1,
         "name": "Harry potter",
         "author": "avijeet",
         "category": "sci-fi"
     },
     {
+        "id": 2,
         "name": "Dead man's tale",
         "author": "Nitin",
         "category": "sci-fi"
     },
     {
+        "id": 3,
         "name": "Pyjama Party",
         "author": "pooja",
         "category": "sci-fi"
     },
     {
+        "id": 4,
         "name": "Harry potter",
         "author": "avijeet",
         "category": "sci-fi"
     },
     {
+        "id": 5,
         "name": "Harry potter",
         "author": "avijeet",
         "category": "sci-fi"
@@ -34,3 +39,13 @@ BOOKS = [
 @app.get("/books")
 async def get_books():
     return {"data": BOOKS}
+
+
+@app.get("/book/{id}")
+async def get_book_by_id(id):
+    # iterating over book and returning
+    for book in BOOKS:
+        if str(book["id"]) == id:
+            print(book["id"])
+            return {"data": book}
+    return {"message": "book with id not found"}
