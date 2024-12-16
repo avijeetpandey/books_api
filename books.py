be7a0b06457b6c5699fc26c3903e3a9da708face
Avijeet <avijeetpandey25@gmail.com>
2024-12-16 09:47:02 +0530

    add: update and delete functionality for a book

--- books.py
+++ books.py
@@ -1,7 +1,8 @@
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, HTTPException
 
 app = FastAPI()
 
+# in memory database
 BOOKS = [
     {
         "id": 1,
@@ -36,11 +37,13 @@ BOOKS = [
 ]
 
 
+# fetch all the books
 @app.get("/books")
 async def get_books():
     return {"data": BOOKS}
 
 
+# fetch book by id
 @app.get("/book/{id}")
 async def get_book_by_id(id):
     # iterating over book and returning
@@ -51,7 +54,41 @@ async def get_book_by_id(id):
     return {"message": "book with id not found"}
 
 
+# create book
 @app.post("/books")
 async def create_book(book=Body()):
     BOOKS.append(book)
     return {"message": "successfully done"}
+
+# update book
+
+
+@app.put("/book/update/{id}")
+async def update_book_by_id(id, updated_book=Body()):
+    book_id = int(id)
+    for book in BOOKS:
+        if book["id"] == book_id:
+            book["name"] = updated_book.get("name")
+            book["author"] = updated_book.get("author")
+            book["category"] = updated_book.get("category")
+
+            # return updated book
+            return {
+                "message": "info updated",
+                "data": book
+            }
+
+    return {"message": "unable to update book"}
+
+
+# delete book
+@app.delete("/book/{id}")
+async def delete_book_by_id(id):
+    book_id = int(id)
+    global BOOKS
+    for book in BOOKS:
+        if book["id"] == book_id:
+            BOOKS = [b for b in BOOKS if b["id"] != book_id]
+            return {"message": f"Book with ID {book_id} has been deleted."}
+
+    raise HTTPException(status_code=404, detail="Book not found")
